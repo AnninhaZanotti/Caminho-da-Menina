a1c1e8c3f226d1ebd773cdb68a58097ba0e1ecaf
Aninha Zanotti <annamariazanotti@gmail.com>
2023-07-05 10:49:28 -0300

    Botão Start funcionando

---
+++ Caminho da Menina/Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string nomeCena;
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ativando painel do menu
+        menuPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //funcao do botao start
+
+    public void StartGame()
+    {
+        //carregando a cena Game
+        SceneManager.LoadScene(nomeCena);
+    }
+}
