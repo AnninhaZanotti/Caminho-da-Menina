8cf3be2d2a99f83e31b6c2f50969f1666076a934
Aninha Zanotti <annamariazanotti@gmail.com>
2023-08-02 08:42:33 -0300

    Inserindo collider no Player e no TileMap, Criando script do player.

--- Caminho da Menina/Assets/Scripts/Player.cs
+++ Caminho da Menina/Assets/Scripts/Player.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+    //variavel para o rigidBody
+    private Rigidbody2D rig;
+    //vetor da posicao Inicial do Player
+    private Vector3 posInicial;
+    //variavel da velocidade
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        posInicial = new Vector3(-1.7, 0.89, 0);
+        transform.position = posInicial;
     }
 
     // Update is called once per frame
